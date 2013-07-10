f809dae903a2a70a7e97c9b7bee14a9a0cacc62e
ibokan <ibokan@ibokan104-xiangqiong.local>
2013-07-10 17:51:07 +0800

    Initial Commit

---
+++ XiamenBoku/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  XiamenBoku
+//
+//  Created by ibokan on 13-7-10.
+//  Copyright (c) 2013年 ibokan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ XiamenBoku/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  XiamenBoku
+//
+//  Created by ibokan on 13-7-10.
+//  Copyright (c) 2013年 ibokan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
